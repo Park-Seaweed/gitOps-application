9ed440317dfb4af10a2834c192cc5f234b4ec981
Park-Seaweed <qkralsgur721@gmail.com>
2023-04-22 22:08:50 +0900

    버전 3 테스트

--- routes/root.js
+++ routes/root.js
@@ -2,6 +2,6 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    reply.code(200).send({ server: "버전 2" })
+    reply.code(200).send({ server: "버전 3" })
   })
 }
