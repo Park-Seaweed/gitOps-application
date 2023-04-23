8a2cf940e7dbf161da0cd230eb650021f4a3fc62
Park-Seaweed <qkralsgur721@gmail.com>
2023-04-23 19:04:40 +0900

    버전 테스트 5

---
+++ routes/articels/index.js
@@ -0,0 +1,40 @@
+const mysql = require('mysql2/promise');
+
+const connection = await mysql.createPool({
+    host: 'mysql-service',
+    user: 'admin',
+    password: 'root',
+    database: 'mydb'
+  });
+
+
+module.exports = async function (fastify, opts){
+    fastify.post('/', async (request, reply) => {
+        const { title, content } = request.body;
+      
+
+        const sql = 'INSERT INTO posts (title, content) VALUES (?, ?)';
+        const [result] = await connection.execute(sql, [title, content]);
+        console.log('New post added to the database!');
+        reply.send('New post added to the database!');
+      });
+
+    fastify.get('/', async (request, reply) => {
+
+      const sql = 'SELECT * FROM posts';
+      const [rows] = await connection.execute(sql);
+      reply.send(rows);
+    });
+    
+
+    fastify.get('/:id', async (request, reply) => {
+      const { id } = request.params;
+    
+      const sql = 'SELECT * FROM posts WHERE id = ?';
+      const [rows] = await connection.execute(sql, [id]);
+      reply.send(rows[0]);
+    });
+}
+
+
+
